0e8263beebc66071cd82d0a8f24d66341aa93f39
hidiyatullin <art_78@mail.ru>
2023-02-14 20:20:48 +0300

    fix: TRY8 Docker

--- gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -3,11 +3,8 @@ package ru.practicum.shareit.request.dto;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
 import ru.practicum.shareit.item.dto.Create;
-import ru.practicum.shareit.user.model.User;
 
 import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
@@ -20,21 +17,4 @@ public class ItemRequestDto {
     @NotBlank(groups = Create.class)
     @Length(max = 200)
     private String description;
-    private User requester;
-    private LocalDateTime created;
-    private List<ItemAnswerDto> items;
-
-    @Getter
-    @Setter
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Builder
-    public static class ItemAnswerDto {
-        private Long id;
-        private String name;
-        private String description;
-        private Long idOwner;
-        private boolean available;
-        private Long requestId;
-    }
 }
